4841379dcaaba37f19bb894da86a4b60db5d5839
EC2 Default User <ec2-user@ip-172-31-26-149.ap-northeast-1.compute.internal>
2023-05-17 11:05:13 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,49 +1,37 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
-    @books = Book.all
-    @book = Book.new
+    @book = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
-    #findメソッドは引数を受け取り、idカラムを引数と比べてレコードを取得してくるメソッド
   end
 
   def edit
-    @book = Book.find(params[:id])
+
   end
 
   def create
     @book = Book.new(book_params)
-    if @book.save
-      flash[:notice]="Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      render :index
-    end
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def update
-     @book = Book.find(params[:id])
-     if @book.update(book_params)
-       flash[:notice]="Book was successfully updated."
-       redirect_to book_path(@book.id)
-     else
-       render :edit
-     end
+
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+
   end
 
   private
-  #ストロングパラメータ
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books' => 'books#index', as: 'index_book'
-  get 'books/:id' => 'books#show', as: 'book'#as:オプション「'books#show'の設定を、bookとして利用できる」
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  post 'books' => 'books#create'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
